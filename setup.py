6cecf2a0ccf16dfbbe41e6f44e4d2daadaf7558f
Antoine Prouvost <antoine.prouvost.2013_dev@polytechnique.org>
2017-09-18 10:25:44 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name='Guard',
+    version='0.1',
+    packages=['guard'],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
